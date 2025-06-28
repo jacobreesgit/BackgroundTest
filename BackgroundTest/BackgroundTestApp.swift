f084d40a4cba42fdb97ff314abdfac3f3ba2abbe
jacobreesgit <jacobrees@me.com>
2025-06-28 13:22:20 +0100

    Initial Commit

---
+++ BackgroundTest/BackgroundTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BackgroundTestApp.swift
+//  BackgroundTest
+//
+//  Created by Jacob Rees on 28/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct BackgroundTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
